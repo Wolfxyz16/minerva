a2ddd53af1bab7c38ff1d0a3ea16d1657fb14ecd
Yeray Li Loaiza <yerayliloaiza@gmail.com>
2022-04-05 20:20:26 +0200

    estilos en categorias

--- resources/js/views/fisica/FisicaInicio.vue
+++ resources/js/views/fisica/FisicaInicio.vue
@@ -1,13 +1,44 @@
 <template>
-    <h1>Física</h1>
-    <h3>Categorias</h3>
-    <ul>
-        <li><a href="#app">Gravitación</a></li>
-        <li><a href="#app">Electróstatica</a></li>
-        <li><a href="#app">Magnetismo e inducción</a></li>
-        <li><a href="#app">Ondas</a></li>
-        <li><a href="#app">Óptica</a></li>
-        <li><a href="#app">Luz y ondas electromágneticas</a></li>
-        <li><a href="#app">Física cuántica y nuclear</a></li>
-    </ul>
+    <main>
+        
+        <h1>Física</h1>
+        <ul>
+            <h3>Categorias</h3>
+            <li><a href="#app">Gravitación</a></li>
+            <li><a href="#app">Electróstatica</a></li>
+            <li><a href="#app">Magnetismo e inducción</a></li>
+            <li><a href="#app">Ondas</a></li>
+            <li><a href="#app">Óptica</a></li>
+            <li><a href="#app">Luz y ondas electromágneticas</a></li>
+            <li><a href="#app">Física cuántica y nuclear</a></li>
+        </ul>
+    
+    </main>
 </template>
+
+<style lang="scss" scoped>
+    ul {
+        min-width: 100%;
+        background: rgba(25, 25, 25, .3);
+        list-style: none;
+        padding: 0;
+
+        h3 {
+            min-width: 100%;
+            text-align: center;
+        }
+
+        li {
+            min-width: 100%;
+            padding: 1rem 0;
+            text-align:  center;
+            a {
+                color: white;
+                min-width: 100%;
+                :hover {
+
+                }
+            }
+        }
+    }
+</style>
